916f125a7c6a47c32fb187b0dfaf8b787c3d2327
alice5311 <150638195+alice5311@users.noreply.github.com>
2024-04-29 09:34:04 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth,windowHeight);
+  apture = createCapture(VIDEO)
+  capture.size(320,240);//設定顯示畫面大小
+  image(capture,mouseX, mouseY)
 }
 
 function draw() {
   background(220);
+  push()
+  translate(width/2-160,height/2-120)
+  image(capture,0,0)
+  pop()
+  
 }
